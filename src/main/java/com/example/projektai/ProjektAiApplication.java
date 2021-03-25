2782565441380f76f468e7b212c9e04a7983e021
MICZU-KOMPUTER\Miczu <miczu0607@gmail.com>
2021-03-25 23:20:58 +0100

    Innitial commit

---
+++ src/main/java/com/example/projektai/ProjektAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektAiApplication.class, args);
+	}
+
+}
